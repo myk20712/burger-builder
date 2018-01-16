115bbce96025f39d6b8fb189de8296ef30ad138f
MAYANK <mpatel2712@gmail.com>
2018-01-16 14:15:37 -0500

    initial start

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Aux from '../../hoc/Auxiliary';
+import classes from './Layout.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
+
+const layout = (props) => (
+    <Aux>
+    <Toolbar />
+    <SideDrawer />
+    <main className={classes.Container}>
+        {props.children}
+    </main>
+    </Aux>
+
+);
+
+export default layout;
